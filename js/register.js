84eefd4ab4b912011a2a2bbcbaee2c835ea6542d
原佳腾 <1325675795@qq.com>
2018-06-14 16:51:52 +0800

    '3'

--- js/register.js
+++ js/register.js
@@ -2,5 +2,10 @@ $(function(){
     $('.jump_index').click(function(){
          $('.agree').css({display:'block'});
          $(this).html('注册');
+        $('.jump_index').click(function(){
+            console.log($(this).html())
+            $(this).attr("href","index.html");  //将连接的href属性修改成要修改的地址
+        })
     })
+
 })
